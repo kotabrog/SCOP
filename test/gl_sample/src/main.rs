a556812ee1d6d0ebef6b2a2497ba802643cdb560
kotabrog <suzuki.kota0331@gmail.com>
2023-03-01 20:21:28 +0900

    update gl_sample

--- test/gl_sample/src/main.rs
+++ test/gl_sample/src/main.rs
@@ -2,12 +2,41 @@
 
 extern crate sdl2;
 extern crate gl;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate render_gl_derive;
 
 pub mod render_gl;
+pub mod resources;
+
+use failure::err_msg;
+use std::path::Path;
+
+use resources::Resources;
+use render_gl::data;
+
 
 fn main() {
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    if let Err(e) = run() {
+        println!("{}", failure_to_string(e));
+    }
+}
+
+#[derive(VertexAttribPointers, Copy, Clone, Debug)]
+#[repr(C, packed)]
+struct Vertex {
+    #[location = "0"]
+    pos: data::f32_f32_f32,
+    #[location = "1"]
+    clr: data::f32_f32_f32,
+}
+
+fn run() -> Result<(), failure::Error> {
+    let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
+
+    let sdl = sdl2::init().map_err(err_msg)?;
+    let video_subsystem = sdl.video().map_err(err_msg)?;
 
     let gl_attr = video_subsystem.gl_attr();
 
@@ -18,79 +47,59 @@ fn main() {
         .window("Test", 900, 700)
         .opengl()
         .resizable()
-        .build()
-        .unwrap();
+        .build()?;
 
-    let gl_context = window.gl_create_context().unwrap();
+    let gl_context = window.gl_create_context().map_err(err_msg)?;
+    let gl = gl::Gl::load_with(|s| {
+        video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
+    });
 
-    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let shader_program = render_gl::Program::from_res(&gl, &res, "shaders/triangle")?;
 
-    use std::ffi::CString;
-
-    let vert_shader = render_gl::Shader::from_vert_source(
-        &CString::new(include_str!("triangle.vert")).unwrap()
-    ).unwrap();
-
-    let frag_shader = render_gl::Shader::from_frag_source(
-        &CString::new(include_str!("triangle.frag")).unwrap()
-    ).unwrap();
-    
-    let shader_program = render_gl::Program::from_shaders(
-        &[vert_shader, frag_shader]
-    ).unwrap();
-
-    shader_program.set_used();
-
-    let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
+    let vertices: Vec<Vertex> = vec![
+        Vertex { pos: (0.5, -0.5, 0.0).into(),  clr: (1.0, 0.0, 0.0).into() }, // bottom right
+        Vertex { pos: (-0.5, -0.5, 0.0).into(), clr: (0.0, 1.0, 0.0).into() }, // bottom left
+        Vertex { pos: (0.0,  0.5, 0.0).into(),  clr: (0.0, 0.0, 1.0).into() }  // top
     ];
 
     let mut vbo: gl::types::GLuint = 0;
     unsafe {
-        gl::GenBuffers(1, &mut vbo);
+        gl.GenBuffers(1, &mut vbo);
     }
 
     unsafe {
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
+        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+        // gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl.BufferData(
             gl::ARRAY_BUFFER, // target
-            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            (vertices.len() * std::mem::size_of::<Vertex>()) as gl::types::GLsizeiptr, // size of data in bytes
             vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
             gl::STATIC_DRAW, // usage
         );
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+        gl.BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
     }
 
     let mut vao: gl::types::GLuint = 0;
     unsafe {
-        gl::GenVertexArrays(1, &mut vao);
+        gl.GenVertexArrays(1, &mut vao);
     }
 
     unsafe {
-        gl::BindVertexArray(vao);
-        
-        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            0, // index of the generic vertex attribute ("layout (location = 0)")
-            3, // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            std::ptr::null() // offset of the first component
-        );
+        gl.BindVertexArray(vao);
+        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
 
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
+        Vertex::vertex_attrib_pointers(&gl);
+
+        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl.BindVertexArray(0);
     }
 
     unsafe {
-        gl::Viewport(0, 0, 900, 700);
-        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+        gl.Viewport(0, 0, 900, 700);
+        gl.ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
-    let mut event_pump = sdl.event_pump().unwrap();
+    let mut event_pump = sdl.event_pump().map_err(err_msg)?;
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -100,13 +109,13 @@ fn main() {
         }
 
         unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl.Clear(gl::COLOR_BUFFER_BIT);
         }
 
         shader_program.set_used();
         unsafe {
-            gl::BindVertexArray(vao);
-            gl::DrawArrays(
+            gl.BindVertexArray(vao);
+            gl.DrawArrays(
                 gl::TRIANGLES, // mode
                 0, // starting index in the enabled arrays
                 3 // number of indices to be rendered
@@ -115,5 +124,31 @@ fn main() {
 
         window.gl_swap_window();
     }
+
+    Ok(())
 }
 
+pub fn failure_to_string(e: failure::Error) -> String {
+    use std::fmt::Write;
+
+    let mut result = String::new();
+
+    for (i, cause) in e.iter_chain().collect::<Vec<_>>().into_iter().rev().enumerate() {
+        if i > 0 {
+            let _ = writeln!(&mut result, "   Which caused the following issue:");
+        }
+        let _ = write!(&mut result, "{}", cause);
+        if let Some(backtrace) = cause.backtrace() {
+            let backtrace_str = format!("{}", backtrace);
+            if backtrace_str.len() > 0 {
+                let _ = writeln!(&mut result, " This happened at {}", backtrace);
+            } else {
+                let _ = writeln!(&mut result);
+            }
+        } else {
+            let _ = writeln!(&mut result);
+        }
+    }
+
+    result
+}
